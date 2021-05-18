3c800dc3f266535fd3643f7532447cbe9965625d
张衒 <zx263510238@yahoo.co.jp>
2021-05-18 18:16:27 +0800

    创建用户管理模块

---
+++ sample-user-manage/src/main/java/com/SampleProject/user/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.SampleProject.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
